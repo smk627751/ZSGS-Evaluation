51e610c7c5f43a68e63ca5a0e55a9dcff13cea81
smk627751 <smk627751@gmail.com>
2023-11-10 15:09:47 +0530

    first commit

---
+++ Jumbled number/src/com/smk627751/jumblednumber/JumbledNumber.java
@@ -0,0 +1,32 @@
+package com.smk627751.jumblednumber;
+
+import java.util.Scanner;
+
+public class JumbledNumber {
+	public static boolean isJumbled(int num)
+	{
+		int prev = 0;
+
+		while(num != 0)
+		{
+			int digit = num % 10;
+			if(digit == prev - 1 || digit == prev + 1)
+			{
+				return true;
+			}
+			num = num / 10;
+			prev = digit;
+		}
+		return false;
+	}
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		if(isJumbled(num))
+		{
+			System.out.print( num + " is a jumbled number");
+		}
+		else System.out.print( num + " is a not jumbled number");
+	}
+}
